7bc9bf46917bed33e76eb156f98c2e6b8f0eb63f
jassonnequin08 <60465567+jassonnequin08@users.noreply.github.com>
2020-01-30 17:32:40 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::any('exercise-1', 'Exercise1');
-Route::any('exercise-2', 'Exercise2');
-Route::any('exercise-3', 'Exercise3@index');
-Route::any('exercise-3/store', 'Exercise3@store');
-Route::any('exercise-final', 'ExerciseFinal@login');
-Route::any('exercise-final/store', 'ExerciseFinal@store');
+Route::any('calc', 'Exercise1');
+Route::any('search', 'Exercise2');
+Route::any('ret', 'Exercise3@index');
+Route::any('save', 'Exercise3@store');
+Route::any('login', 'ExerciseFinal@login');
+Route::any('create', 'ExerciseFinal@store');
